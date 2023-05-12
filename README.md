f2ca1c1c634f9d11b48403a358661f1b19377634
Zakarya Meddad <zakarya.meddad@gmail.com>
2023-05-12 19:19:12 +0100

    added README, added collapsable

---
+++ README.md
@@ -0,0 +1,13 @@
+# Activité 4
+Description :
+* Le travail doit se faire en groupe de 10 étudiants.
+* Chaque deux étudiants est responsable d’une section du rapport.
+* Le rapport doit inclure les sections suivantes :
+	* Section 1 : Résume de chapitre 1,
+	* Section 2 : Résume de chapitre 2,
+	* Section 3 : Résume de chapitre 3,
+	* Section 4 : Résume de chapitre 4,
+	* Section 5 : Résume de chapitre 5,
+* Le rapport doit être présenté sous format html (vous pouvez également
+utiliser css).
+* Le fichier html doit être mis sur un repo public sur Github. 
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-let xhr = new XMLHttpRequest();
-let fileContent = new String();
-xhr.open('GET', 'resumes\\resume_1.html', false);
-xhr.onreadystatechange = function () {
-	if(xhr.readyState == 4 && xhr.status == 200) {
-		fileContent = xhr.responseText;
-	}
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
-xhr.send();
-document.getElementById('resume1').innerHTML = fileContent;
